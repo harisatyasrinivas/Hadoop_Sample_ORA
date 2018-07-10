4e02a53a96cf285decf0495bf1ad4b08f5be43e3
harisatyasrinivas <harisatyasrinivas@gmail.com>
2018-07-10 00:04:45 -0400

    created sample file

---
+++ sample.py
@@ -0,0 +1,17 @@
+﻿Opening repositories:
+C:\Users\haris\Source\Repos\MyHadoop
+Opening repositories:
+S:\IT_Simulations\python-spark-tutorial
+Opening repositories:
+C:\Users\haris\Source\Repos\MyHadoop
+Remote: Counting objects: 2, done.        
+Remote: Total 2 (delta 0), reused 0 (delta 0), pack-reused 2        
+Commit 51718963 created locally in repository C:\Users\haris\Source\Repos\MyHadoop
+Error: failed to push some refs to 'https://github.com/harisatyasrinivas/MyHadoop.git'
+Error: hint: Updates were rejected because the remote contains work that you do
+hint: not have locally. This is usually caused by another repository pushing
+hint: to the same ref. You may want to first integrate the remote changes
+hint: (e.g., 'git pull ...') before pushing again.
+hint: See the 'Note about fast-forwards' in 'git push --help' for details.
+
+Error encountered while pushing to the remote repository: rejected Updates were rejected because the remote contains work that you do not have locally. This is usually caused by another repository pushing to the same ref. You may want to first integrate the remote changes before pushing again.
